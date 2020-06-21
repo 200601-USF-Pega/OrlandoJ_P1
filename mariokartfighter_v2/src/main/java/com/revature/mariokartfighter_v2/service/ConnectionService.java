26440403ad41c5b1abf1d519a162eac64c9d5877
jorlando989 <jorlando989@gmail.com>
2020-06-20 23:34:48 -0400

    fix connection service

--- mariokartfighter_v2/src/main/java/com/revature/mariokartfighter_v2/service/ConnectionService.java
+++ mariokartfighter_v2/src/main/java/com/revature/mariokartfighter_v2/service/ConnectionService.java
@@ -12,16 +12,19 @@ public class ConnectionService {
 	
 	public ConnectionService() {
 		try {
-			FileInputStream fis = new FileInputStream("C:/Users/jorla/Desktop/Revature Training/MarioKartFighter_v2/mariokartfighter_v2/connection.prop");
-			Properties p = new Properties();
-			p.load(fis);
-			
-			connection = DriverManager.getConnection(p.getProperty("hostname"), 
-					p.getProperty("username"), p.getProperty("password"));
-		} catch(SQLException e) {
+			if (connection == null || connection.isClosed()) {
+	            try {
+	                FileInputStream fis = new FileInputStream("C:\\Users\\jorla\\Desktop\\Revature Training\\MarioKartFighter_v2\\mariokartfighter_v2\\connection.prop");
+	                Properties p = new Properties();
+	                p.load(fis);
+	                connection = DriverManager.getConnection(p.getProperty("hostname"), p.getProperty("username"), p.getProperty("password"));
+	            }catch (IOException e) {
+	                System.out.println("Could not get connection!");
+	                System.out.println(e.getMessage());
+	            }
+	        }
+		} catch (SQLException e) {
 			System.out.println("Exception: " + e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
